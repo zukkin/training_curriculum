e0937f49d198eb601507676983d3c64f0652ba67
zukkin <73934559+zukkin@users.noreply.github.com>
2020-11-21 13:56:32 +0900

    Isuue4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plans).permit(:date, :plan)
   end
 
   def getWeek
